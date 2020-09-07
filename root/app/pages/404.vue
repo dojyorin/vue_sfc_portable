bfeac0913b868ae61d8c25204e6d74dc07e61298
dojyorin <ota_droid@live.jp>
2020-09-08 01:24:41 +0900

    fix: height source.

--- root/app/pages/404.vue
+++ root/app/pages/404.vue
@@ -1,5 +1,5 @@
 <template>
-<v-row justify="center" align="center" class="text-h4" :style="`height:calc(100vh - ${$vuetify.application.top}px)!important;`">
+<v-row justify="center" align="center" class="text-h4" :style="`height:calc(${$vuetify.breakpoint.height}px - ${$vuetify.application.top}px)!important;`">
     Page Not Found ...
 </v-row>
 </template>
