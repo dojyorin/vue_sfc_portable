1b3b11ef35ff6663c46aad6a24fa3297699231f9
dojyorin <ota_droid@live.jp>
2020-09-12 22:54:16 +0900

    update.

--- root/app/components/layouts/footer.vue
+++ root/app/components/layouts/footer.vue
@@ -1,7 +1,5 @@
 <template>
-<v-row justify="center">
-    <v-card flat tile>
-        <v-card-text class="pa-0">Copyright</v-card-text>
-    </v-card>
+<v-row justify="center" align="center">
+    Copyright
 </v-row>
 </template>
