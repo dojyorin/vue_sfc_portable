aaddd6180b8bed00145656959168c3676194da3d
dojyorin <ota_droid@live.jp>
2020-09-15 21:28:37 +0900

    update.

--- root/app/store/store.mjs
+++ root/app/store/store.mjs
@@ -1,14 +1,11 @@
-import {namespaced, state, getters, mutations, actions} from "./index.mjs";
-import main from "./main.mjs";
+import index from "./index.mjs";
+import user from "./user.mjs";
+
+const modules = {
+    "": index,
+    "user": user
+};
 
 export default {
-    namespaced,
-    state,
-    getters,
-    mutations,
-    actions,
-
-    modules: {
-        main
-    }
+    modules
 };
