a16729b636f285fa4e30e60af232fa018def92ea
dojyorin <ota_droid@live.jp>
2021-06-14 17:44:15 +0900

    wording.

--- root/application/stores/main.mjs
+++ root/application/stores/main.mjs
@@ -23,17 +23,18 @@ export default {
 
     actions: {
         vNotify(ctx, payload){
-            const handler = setTimeout(()=>{
-                const index = ctx.getters.getNotifies.findIndex(({token}) => token === handler);
-                if(index >= 0){
-                    ctx.commit("spliceNotify", index);
+            const timer = setTimeout(()=>{
+                const index = ctx.getters.getNotifies.findIndex(({token}) => token === timer);
+                if(index < 0){
+                    return;
                 }
+                ctx.commit("spliceNotify", index);
             }, payload?.timeout || 5000);
 
             ctx.commit("pushNotify", {
                 message: payload.message,
                 color: payload.color,
-                token: handler
+                token: timer
             });
         }
     }
