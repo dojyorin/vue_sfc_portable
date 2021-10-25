2bf93c85da23da423c4cc13ff4a07af9751da338
dojyorin <ota_droid@live.jp>
2021-10-25 22:42:31 +0900

    small fix.

--- src/core/fetch_mod.js
+++ src/core/fetch_mod.js
@@ -12,17 +12,17 @@ Object.defineProperty(globalThis, "$fetch", {
                 return h;
             }, new Headers()),
             method: option?.method ?? "get",
+            credentials: option?.credentials ?? "omit",
+            body: option?.body ?? null,
             mode: option?.mode ?? "cors",
             cache: option?.cache ?? "no-cache",
-            credentials: option?.credentials ?? "omit",
             referrerPolicy: option?.referrerPolicy ?? "no-referrer",
             referrer: option?.referrer ?? "",
             redirect: option?.redirect ?? "follow",
             keepalive: option?.keepalive ?? false,
             integrity: option?.integrity ?? "",
             signal: option?.signal ?? null,
-            window: null,
-            body: option?.body ?? null
+            window: null
         }))[option?.type ?? "json"]();
     }
 });
