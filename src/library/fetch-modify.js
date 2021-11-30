54b4e213f24d4b54842946e529f1c2d693668f0c
dojyorin <ota_droid@live.jp>
2021-11-30 23:21:52 +0900

    code update.

--- src/library/fetch-modify.js
+++ src/library/fetch-modify.js
@@ -2,27 +2,24 @@ Object.defineProperty(globalThis, "$fetch", {
     enumerable: false,
     configurable: false,
     writable: false,
-    async value(url, option){
-        return (await fetch(Object.entries(option?.params ?? {}).reduce((u, [k, v])=>{
-            u.searchParams.append(k, v);
-            return u;
-        }, new URL(url, /^http(s|):\/\//i.test(url) ? undefined : location.href)), {
-            headers: Object.entries(option?.headers ?? {}).reduce((h, [k, v])=>{
-                h.append(k, v);
-                return h;
-            }, new Headers()),
+    async value(path, option){
+        return (await fetch(Object.entries(option?.params ?? {}).reduce((url, [key, value])=>{
+            url.searchParams.append(key, value);
+            return url;
+        }, new URL(path, /^http(s|):\/\//i.test(path) ? undefined : location.href)), {
             method: option?.method ?? "get",
             credentials: option?.credentials ?? "omit",
-            body: option?.body ?? null,
             mode: option?.mode ?? "cors",
             cache: option?.cache ?? "no-cache",
             referrerPolicy: option?.referrerPolicy ?? "no-referrer",
             referrer: option?.referrer ?? "",
             redirect: option?.redirect ?? "follow",
             keepalive: option?.keepalive ?? false,
-            integrity: option?.integrity ?? "",
-            signal: option?.signal ?? null,
-            window: null
+            integrity: "",
+            signal: null,
+            window: null,
+            headers: option?.headers ?? {},
+            body: option?.body ?? null
         }))[option?.type ?? "json"]();
     }
 });
