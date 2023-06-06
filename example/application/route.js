13378be9ed24a2818b1c84134ee9ca525a7f359e
Kazuki Ota <ota_droid@live.jp>
2023-06-06 22:25:48 +0900

    fix.

--- example/application/route.js
+++ example/application/route.js
@@ -1,16 +1,16 @@
 import {createRouter, createWebHashHistory} from "vue-router";
-import {asyncComponent} from "../deps.js";
+import {fetchAsyncComponent} from "../deps.js";
 
 export const route = createRouter({
     history: createWebHashHistory(),
     routes: [{
         path : "/",
-        component: asyncComponent("./application/page/home.vue")
+        component: fetchAsyncComponent("./application/page/home.vue")
     }, {
         path : "/sub",
-        component: asyncComponent("./application/page/sub.vue")
+        component: fetchAsyncComponent("./application/page/sub.vue")
     }, {
         path : "/:catchAll(.*)",
-        component: asyncComponent("./application/page/404.vue")
+        component: fetchAsyncComponent("./application/page/404.vue")
     }]
 });
