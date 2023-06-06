4279e107ed7927cd15e52a187bb90586a5d2110b
Kazuki Ota <ota_droid@live.jp>
2023-06-06 15:51:47 +0900

    fix url.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export {type FetchInit, fetchExtend, randomUuid} from "https://deno.land/x/simple_utility@v0.5.2/mod.universal.ts";
+// @deno-types="https://deno.land/x/simple_utility@v0.5.2/mod.universal.ts"
+export {type FetchInit, fetchExtend, randomUuid} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v0.5.2/mod.universal.ts?bundle&target=esnext";
 export {type Component, defineAsyncComponent} from "https://esm.sh/vue@3.3.4?bundle&target=esnext";
