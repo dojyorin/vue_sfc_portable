dfae6d64a5f2a8c8a740a7cca4d0b5662903ddf3
Kazuki Ota <ota_droid@live.jp>
2023-06-08 21:06:06 +0900

    navlink.

--- example/application/layout/nav.vue
+++ example/application/layout/nav.vue
@@ -1,2 +1,11 @@
 <template>
+    <v-list>
+        <v-list-item to="/">
+            <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item to="/sub">
+            <v-list-item-title>Sub</v-list-item-title>
+        </v-list-item>
+    </v-list>
 </template>
