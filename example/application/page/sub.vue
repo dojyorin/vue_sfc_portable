04558054053f1e8b250fcf1e7c8273be2f126855
Kazuki Ota <ota_droid@live.jp>
2023-06-08 19:24:20 +0900

    remove vue2 component.

--- example/application/page/sub.vue
+++ example/application/page/sub.vue
@@ -7,42 +7,6 @@
 
             <v-list-item-title>Title</v-list-item-title>
             <v-list-item-subtitle>Sub</v-list-item-subtitle>
-
-            <!-- <v-list-item-action>
-                <ct-dialog persistent maximize color="purple" title="Dialog" icon="mdi-information-outline" v-model="dialog">
-                    <template #activator>
-                        <v-btn icon @click.stop="dialog = true">
-                            <v-icon>mdi-information-outline</v-icon>
-                        </v-btn>
-                    </template>
-
-                    <v-card>
-                        <v-card-text>Contents</v-card-text>
-                    </v-card>
-                </ct-dialog>
-            </v-list-item-action> -->
         </v-list-item>
     </v-list>
 </template>
-
-<script>
-    import {defineComponent, defineAsyncComponent, fetchComponent} from "../../deps.js";
-
-    export default {
-        // components: {
-        //     "ct-dialog": fetchAsyncComponent("../component/dialog.vue")
-        // },
-
-        // data(){
-        //     return {
-        //         dialog: false
-        //     };
-        // },
-
-        // async mounted(){
-        //     this.$store.commit("loadingVisible", true);
-        //     await new Promise(res => setTimeout(res, 1000));
-        //     this.$store.commit("loadingVisible", false);
-        // }
-    };
-</script>
