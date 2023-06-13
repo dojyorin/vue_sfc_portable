16506201cf21d48bbc1142d78babf9133b8c5578
Kazuki Ota <ota_droid@live.jp>
2023-06-13 16:16:09 +0900

    update import.

--- src/compile.ts
+++ src/compile.ts
@@ -3,31 +3,48 @@
 /// <reference lib="dom"/>
 /// <reference lib="dom.iterable"/>
 
-import {type Component} from "../deps.ts";
+import {type Component, randomBin, hexEncode} from "../deps.ts";
 
-interface SFCPart{
-    template: HTMLTemplateElement;
-    script?: HTMLScriptElement;
-    style?: HTMLStyleElement;
+function findPart<T extends typeof HTMLElement>(elements:Element[], type:T){
+    return <InstanceType<T> | undefined>elements.find(e => e instanceof type);
 }
 
-const AsyncFunction = <FunctionConstructor>(async function(){}).constructor;
-
-export function importComponent(sfc:string):SFCPart{
+export async function compileComponent(sfc:string, path?:string):Promise<Component>{
     const {head: {children: [...elements]}} = new DOMParser().parseFromString(sfc, "text/html");
 
-    const template = <HTMLTemplateElement | undefined>elements.find(e => e instanceof HTMLTemplateElement);
-    const script = <HTMLScriptElement | undefined>elements.find(e => e instanceof HTMLScriptElement);
-    const style = <HTMLStyleElement | undefined>elements.find(e => e instanceof HTMLStyleElement);
+    const template = findPart(elements, HTMLTemplateElement);
+    const script = findPart(elements, HTMLScriptElement);
+    const style = findPart(elements, HTMLStyleElement);
 
     if(!template){
         throw new Error();
     }
 
-    return {template, script, style};
-}
+    if(script?.innerHTML){
+        script.innerHTML = script.innerHTML.replace(/"\.{0,2}\/(\\"|[^"\r\n\t ])+"|'\.{0,2}\/(\\'|[^'\r\n\t ])+'|`\.{0,2}\/(\\`|[^`\r\n\t ])+`/g, (sub)=>{
+            const [quote] = sub;
+            const name = sub.replace(/^["'`]/, "").replace(/["'`]$/, "");
 
-export async function exportComponent({template, script, style}:SFCPart):Promise<Component>{
+            return `${quote}${decodeURIComponent(new URL(name, new URL(path ?? "", location.href)).href).replace(new RegExp(quote, "g"), `\\${quote}`)}${quote}`;
+        });
+    }
+
+    if(style?.innerHTML && style.hasAttribute("scoped")){
+        const scope = `data-v-${hexEncode(randomBin(4))}`;
+
+        for(const {attributes} of template.content.querySelectorAll("[class]")){
+            attributes.setNamedItem(document.createAttribute(scope));
+        }
+
+        for(const rule of style.sheet?.cssRules ?? []){
+            if(!(rule instanceof CSSStyleRule)){
+                continue;
+            }
+
+            rule.selectorText += `[${scope}]`;
+        }
+    }
+
     if(style?.sheet?.cssRules){
         const element = document.createElement("style");
 
@@ -40,6 +57,6 @@ export async function exportComponent({template, script, style}:SFCPart):Promise
 
     return {
         template: template.innerHTML,
-        ...await new AsyncFunction(script?.innerHTML ?? "")() ?? {}
+        ...(await import(`data:text/javascript;base64,${btoa(script?.innerHTML ?? "")}`)).default
     };
 }
