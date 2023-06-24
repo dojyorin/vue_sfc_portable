195074faf2d92d9a33c1ac9c0d009ba94d056522
dojyorin <ota_droid@live.jp>
2023-06-25 01:40:29 +0900

    core module update.

--- example/deps.js
+++ example/deps.js
@@ -1,6 +1,6 @@
-export {createApp, defineComponent, defineAsyncComponent, ref, computed, onMounted} from "vue";
-export {createRouter, createWebHashHistory, useRouter} from "vue-router";
-export {createStore, useStore} from "vuex";
-export {createVuetify} from "vuetify";
+export {createApp, defineComponent, defineAsyncComponent, ref, computed, onMounted} from "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.min.js";
+export {createRouter, createWebHashHistory, useRouter} from "https://cdn.jsdelivr.net/npm/vue-router@4/dist/vue-router.esm-browser.min.js";
+export {createStore, useStore} from "https://cdn.jsdelivr.net/npm/vuex@4/dist/vuex.esm-browser.prod.min.js";
+export {createVuetify} from "https://cdn.jsdelivr.net/npm/vuetify@3/dist/vuetify.esm.min.js";
 
-export {fetchComponent} from "https://esm.sh/gh/dojyorin/vue_sfc_portable@v1.3.2/mod.ts?bundle&target=esnext";
+export {fetchComponent} from "https://esm.sh/gh/dojyorin/vue_sfc_portable@v1.4.0/mod.ts?bundle&target=esnext";
