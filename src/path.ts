2fb88fc7de0e561257aee2fe64f6ceae4d9528b3
Kazuki Ota <ota_droid@live.jp>
2023-06-29 05:49:12 +0900

    update deps.

--- src/path.ts
+++
@@ -1,8 +0,0 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="esnext"/>
-/// <reference lib="dom"/>
-/// <reference lib="dom.iterable"/>
-
-export function resolvePath(base:string, ...paths:string[]){
-    return decodeURIComponent(paths.reduce((a, b) => new URL(b, a).href, base));
-}
