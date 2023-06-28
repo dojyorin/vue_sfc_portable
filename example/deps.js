ce8b9985601d6f477caac26969d60c8d1cea05b1
Kazuki Ota <ota_droid@live.jp>
2023-06-28 17:27:26 +0900

    provide wip.

--- example/deps.js
+++ example/deps.js
@@ -1,5 +1,5 @@
 // @deno-types="https://esm.sh/vue@3?target=esnext"
-export {createApp, defineComponent, defineAsyncComponent, ref, computed, onMounted} from "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.min.js";
+export {createApp, defineComponent, defineAsyncComponent, ref, reactive, inject, computed, onMounted} from "https://cdn.jsdelivr.net/npm/vue@3/dist/vue.esm-browser.prod.min.js";
 // @deno-types="https://esm.sh/vue-router@4?target=esnext"
 export {createRouter, createWebHashHistory, useRouter} from "https://cdn.jsdelivr.net/npm/vue-router@4/dist/vue-router.esm-browser.min.js";
 // @deno-types="https://esm.sh/vuex@4?target=esnext"
---
+++ example/vue/provide.js
@@ -0,0 +1,14 @@
+import {fetchComponent, ref, reactive} from "../deps.js";
+
+export const provide = ((arg)=>{
+    return {
+        install(context){
+            for(const [k, v] of Object.entries(arg)){
+                context.provide(k, v);
+            }
+        }
+    };
+})({
+    "loading": ref(false),
+    "notifies": reactive([])
+});
