0d2fff072afd4c677a913e815f3149f4880fa69f
dojyorin <ota_droid@live.jp>
2023-07-27 19:19:42 +0900

    split module.

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export {type Component} from "https://esm.sh/vue@3.3.4?bundle&target=esnext";
 // @deno-types="https://esm.sh/terser@5.19.2/tools/terser.d.ts"
 export {minify as minifyScript} from "https://esm.sh/terser@5.19.2?bundle&target=esnext";
 
-// @deno-types="https://deno.land/x/simple_utility@v1.3.1/mod.ts"
-export {type FetchInit, type DefExp, fetchExtend, evaluateESM, randomBin, hexEncode} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.3.1/mod.universal.ts?bundle&target=esnext";
+// @deno-types="https://deno.land/x/simple_utility@v1.3.4/mod.ts"
+export {type FetchInit, type DefExp, fetchExtend, evaluateESM, pad0} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.3.4/mod.universal.ts?bundle&target=esnext";
