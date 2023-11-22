6546628558db782942729d4a9ee220e399443b13
dojyorin <ota_droid@live.jp>
2023-11-22 19:16:03 +0900

    update deps.

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export {type Component} from "https://esm.sh/vue@3.3.8?bundle&target=esnext";
 // @deno-types="https://esm.sh/terser@5.24.0/tools/terser.d.ts"
 export {minify} from "https://esm.sh/terser@5.24.0?bundle&target=esnext";
 
-// @deno-types="https://deno.land/x/simple_utility@v1.4.3/mod.ts"
-export {type FetchInit, fetchExtend, b64DataURL, u8Encode, pad0} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.4.3/mod.universal.ts?bundle&target=esnext";
+// @deno-types="https://deno.land/x/simple_utility@v1.4.4/mod.ts"
+export {type FetchInit, fetchExtend, b64DataURL, u8Encode, pad0} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.4.4/mod.universal.ts?bundle&target=esnext";
