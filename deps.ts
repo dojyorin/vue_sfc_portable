9c4c5eedfd404362536ee17e9637510702201b82
Kazuki Ota <ota_droid@live.jp>
2023-12-29 00:46:04 +0900

    update deps. (#37)

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-// @deno-types="https://esm.sh/vue@3.3.11/dist/vue.d.ts"
-export {type Component} from "https://esm.sh/vue@3.3.11?bundle&target=esnext";
+// @deno-types="https://esm.sh/vue@3.3.13/dist/vue.d.ts"
+export {type Component} from "https://esm.sh/vue@3.3.13?bundle&target=esnext";
 
 // @deno-types="https://esm.sh/terser@5.26.0/tools/terser.d.ts"
 export {minify} from "https://esm.sh/terser@5.26.0?bundle&target=esnext";
 
-// @deno-types="https://deno.land/x/simple_utility@v1.5.0/mod.ts"
-export {type FetchInit, fetchExtend, b64DataURL, u8Encode, pad0} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.5.0/mod.universal.ts?bundle&target=esnext";
+// @deno-types="https://deno.land/x/simple_utility@v1.5.2/mod.ts"
+export {type FetchInit, fetchExtend, b64DataURL, u8Encode, pad0} from "https://esm.sh/gh/dojyorin/deno_simple_utility@v1.5.2/mod.universal.ts?bundle&target=esnext";
